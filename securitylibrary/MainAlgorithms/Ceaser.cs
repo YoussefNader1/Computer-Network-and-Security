57b99ef42baf94da91faf1dfe046046f053eb41d
el a5tabot <youssef20191700793@cis.asu.edu.eg>
2023-03-05 19:17:21 +0200

    Add Main algorithms
    Add ceaser
    Add Monoalpahabetic

---
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        public string Encrypt(string plainText, int key)
+        {
+
+            string CT = "";
+            int CT_index = 0;
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if (plainText[i] == alphabet[j])
+                    {
+                        CT_index = ((j + key) % 26);
+                        CT += char.ToUpper(alphabet[CT_index]);
+                        break;
+                    }
+                }
+            }
+
+
+
+            return CT;
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            string PT = "";
+            int PT_index = 0;
+            string lowerCipher = cipherText.ToLower();
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if (lowerCipher[i] == alphabet[j])
+                    {
+                        PT_index = ((j - key)) % 26;
+                        if (PT_index < 0)
+                        {
+                            PT_index += 26;
+                        }
+
+                        PT += alphabet[PT_index];
+                    }
+                }
+            }
+
+            return PT;
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+            int PT_index = 0;
+            int CI_index = 0;
+            string lowerCipher = cipherText.ToLower();
+            char PTchar = plainText[0];
+            char CIchar = lowerCipher[0];
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (PTchar == alphabet[i])
+                {
+                    PT_index = i;
+                }
+                if (CIchar == alphabet[i])
+                {
+                    CI_index = i;
+                }
+            }
+
+            int key = (CI_index - PT_index)% 26;
+            if (key < 0)
+            {
+                key += 26;
+            }
+
+            return key;
+        }
+    }
+}
---
+++ securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Monoalphabetic : ICryptographicTechnique<string, string>
+    {    
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+   
+        public string Analyse(string plainText, string cipherText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt(string cipherText, string key)
+        {
+            string PT = "";
+            string lowerCipher = cipherText.ToLower();
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+            for (int i = 0; i < key.Length; i++)
+            {
+                dic.Add(key[i] , alphabet[i]);
+            }
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                PT += dic[lowerCipher[i]];
+            }
+            return PT;
+        }
+
+        public string Encrypt(string plainText, string key)
+        {
+            string CT = "";
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+            for (int i = 0; i < key.Length; i++)
+            {
+                dic.Add(alphabet[i], key[i]);
+            }
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                CT += dic[plainText[i]];
+            }
+            return CT;
+        }
+
+        /// <summary>
+        /// Frequency Information:
+        /// E   12.51%
+        /// T	9.25
+        /// A	8.04
+        /// O	7.60
+        /// I	7.26
+        /// N	7.09
+        /// S	6.54
+        /// R	6.12
+        /// H	5.49
+        /// L	4.14
+        /// D	3.99
+        /// C	3.06
+        /// U	2.71
+        /// M	2.53
+        /// F	2.30
+        /// P	2.00
+        /// G	1.96
+        /// W	1.92
+        /// Y	1.73
+        /// B	1.54
+        /// V	0.99
+        /// K	0.67
+        /// X	0.19
+        /// J	0.16
+        /// Q	0.11
+        /// Z	0.09
+        /// </summary>
+        /// <param name="cipher"></param>
+        /// <returns>Plain text</returns>
+        public string AnalyseUsingCharFrequency(string cipher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
