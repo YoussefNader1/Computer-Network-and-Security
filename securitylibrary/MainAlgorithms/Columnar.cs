ee4ccbd7604de2b1b1e78503558705677496e3ff
Ahmed Tarek <ahmed.tarek1244@gmail.com>
2023-03-08 21:05:34 +0200

    finish rail fence and columnar

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -10,17 +10,144 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            List<int> key = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            String cipher = "";
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            //List<List<int>> x = Permute(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+            for (int i = 4; i < plainText.Length / 3 + 1; i++)
+            {
+                Console.WriteLine(i);
+                List<List<int>> posibleKeys = Permute(numbers(i));
+                for (int j = 0; j < posibleKeys.Count; j++)
+                {
+                    cipher = Encrypt(plainText, posibleKeys[j]);
+                    if (cipher == cipherText)
+                    {
+                        return posibleKeys[j];
+                    }
+                }
+            }
+            return key;
+
         }
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            int cipherLength = (int)Math.Ceiling((float)((float)cipherText.Length / (float)key.Count));
+            char[,] cipher = new char[cipherLength, key.Count];
+            Console.WriteLine("cipher No.of.rows: " + cipherLength + " No.of.columns: " + key.Count);
+
+            int index = 0;
+            int x = 0;
+            for (int i = 0; i < key.Count; i++)
+            {
+                index = key.IndexOf(i + 1);
+                for (int j = 0; j < cipherLength; j++)
+                {
+                    if (x < cipherText.Length)
+                    {
+                        cipher[j, index] = cipherText[x];
+                        x++;
+                    }
+                    Console.WriteLine("r: " + i + " c: " + j + " index: " + x + " => " + cipher[j, index] + '\n');
+                }
+            }
+
+            String plainText = "";
+            for (int i = 0; i < cipherLength; i++)
+            {
+                for (int j = 0; j < key.Count; j++)
+                {
+                    plainText = plainText.Insert(plainText.Length, cipher[i, j].ToString());
+                }
+            }
+            Console.WriteLine(plainText);
+            return plainText;
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            int cipherLength = (int)Math.Ceiling((float)((float)plainText.Length / (float)key.Count));
+            char[,] cipher = new char[cipherLength, key.Count];
+            //Console.WriteLine("cipher No.of.rows: "+cipherLength+" No.of.columns: "+mainkey.Count);
+            int x = 0;
+            for (int i = 0; i < cipherLength; i++)
+            {
+                for (int j = 0; j < key.Count; j++)
+                {
+
+                    if (x >= plainText.Length)
+                    {
+                        //cipher[i, j] = 'x';
+                        //Console.WriteLine("r: " + i + " c: " + j + " index: " + x + " => " + cipher[i, j]);
+                        break;
+                    }
+
+                    cipher[i, j] = plainText[x];
+                    x++;
+                    //Console.WriteLine("r: " + i + " c: " + j + " index: " + x+" => " + cipher[i, j]+'\n');
+                }
+            }
+            String cipherText = "";
+            int index = 0;
+            for (int i = 0; i < key.Count; i++)
+            {
+                index = key.IndexOf(i + 1);
+                for (int j = 0; j < cipherLength; j++)
+                {
+                    cipherText = cipherText.Insert(cipherText.Length, cipher[j, index].ToString());
+                    //Console.WriteLine("r: " + i + " c: " + j + " index: " + x + " => " + cipher[j, index] + '\n');
+                }
+            }
+            Console.WriteLine(cipherText);
+            return cipherText;
         }
+
+        public static List<List<int>> Permute(int[] nums)
+        {
+            var list = new List<List<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        public static List<List<int>> DoPermute(int[] nums, int start, int end, List<List<int>> list)
+        {
+            if (start == end)
+            {
+                // We have one of our possible n! solutions,
+                // add it to the list.
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+        public static int[] numbers(int number)
+        {
+            int[] numbers = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            return numbers;
+        }
+
+
     }
 }
+
--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,84 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            String str = "";
+            int key = 0;
+            for (int i = 2; i < plainText.Length; i++)
+            {
+                str = Encrypt(plainText, i);
+
+                if (str == cipherText)
+                {
+                    return i;
+
+                }
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            char[,] cipher = new char[key, cipherText.Length];
+
+            int x = 0;
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = i; j < cipherText.Length; j = j + key)
+                {
+                    cipher[i, j] = cipherText[x];
+                    x++;
+                    //Console.WriteLine("r: "+i+" c: "+j+" k: "+ cipherText[x]);
+                }
+            }
+
+            String plainText = "";
+            int l = 0;
+            while (l < cipherText.Length)
+            {
+                for (int k = 0; k < key; k++)
+                {
+                    if (l == cipherText.Length)
+                        break;
+                    plainText = plainText.Insert(plainText.Length, cipher[k, l].ToString());
+                    l++;
+                }
+
+            }
+            return plainText;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+
+            char[,] cipher = new char[key, plainText.Length];
+            int j = 0;
+            while (j < (plainText.Length))
+            {
+                for (int i = 0; i < key; i++)
+                {
+                    if (j != (plainText.Length))
+                    {
+                        cipher[i, j] = plainText[j];
+                        j++;
+                    }
+                }
+
+            }
+
+            String cipherText = "";
+            for (int i = 0; i < key; i++)
+            {
+                for (int k = 0; k < plainText.Length; k++)
+                {
+                    if (cipher[i, k] != ' ' && cipher[i, k] != '\0')
+                    {
+                        cipherText = cipherText.Insert(cipherText.Length, cipher[i, k].ToString());
+                    }
+                }
+            }
+            Console.WriteLine(cipherText);
+            return cipherText.ToUpper();
+            //throw new NotImplementedException();
         }
     }
 }
