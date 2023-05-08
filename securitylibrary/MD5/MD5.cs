43f9e9fc8c01cd38fd83aeb43ad61cf0e04210d5
Khaled A. Abdelzaher <khaled3bzaher@gmail.com>
2023-05-08 23:58:40 +0300

    Ready for coding Operation

--- securitylibrary/MD5/MD5.cs
+++ securitylibrary/MD5/MD5.cs
@@ -4,13 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SecurityLibrary.MD5
 {
     public class MD5
     {
         public string GetHash(string text)
         {
-            throw new NotImplementedException();
+            var msgBytes=Encoding.UTF8.GetBytes(text);
+            string txtBIN = "";
+            foreach (byte b in msgBytes)
+            {
+                txtBIN+=Convert.ToString(b, 2).PadLeft(8, '0');
+            }
+            var txtBINLen = Convert.ToString(txtBIN.Length, 2).PadLeft(8, '0');
+            if (txtBIN.Length < 448)
+            {
+                txtBIN += "1";
+                int remLen = 448 - txtBIN.Length+56;
+                for (int i=0;i< remLen; i++)
+                {
+                    txtBIN += "0";
+                }
+                txtBIN += txtBINLen;
+            }
+
+            uint A = 0x67452301;
+            uint B = 0xEFCDAB89;
+            uint C = 0x98BADCFE;
+            uint D = 0x10325476;
+
+            List<uint> T=new List<uint>();
+            for(int i = 1; i <= 64; i++)
+            {
+                T.Add((uint)(Math.Floor(Math.Abs(Math.Sin(i)) * Math.Pow(2, 32))));
+            }
+
+            uint F = (B & C) | ((~B) & D);
+            uint G = (D & B) | ((~D) & C);
+            uint H = B ^ C ^ D;
+            uint I = C ^ (B | (~D));
+
+            return "";
         }
     }
 }
